41d07ea9120b17b4048e238d6388d8357efc5f86
Odai zagha <odai.zagha0962@gmail.com>
2024-05-11 17:25:33 +0300

    changes Done

---
+++ app/src/main/java/com/example/parkingapp/EditProfile.java
@@ -0,0 +1,17 @@
+package com.example.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+    }
+}
