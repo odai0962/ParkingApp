44d141ec70be2d26102dd14ed4349c5268b9a4ea
Odai zagha <odai.zagha0962@gmail.com>
2024-05-13 14:45:12 +0300

    changes Done

---
+++ app/src/main/java/com/example/parkingapp/CarParkDetailes.java
@@ -0,0 +1,14 @@
+package com.example.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarParkDetailes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_park_detailes);
+    }
+}
